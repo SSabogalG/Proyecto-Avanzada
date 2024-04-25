49f01d4087b47b8cca82f866d3deca194d4c6ca1
SSabogalG <sebastian.sabogalg@uqvirtual.edu.co>
2024-04-25 14:17:59 -0500

    Modificación en el login

---
+++ src/app/DTO/inicio-sesion-dto.ts
@@ -0,0 +1,6 @@
+export class InicioSesionDTO {
+    constructor(
+        public correo:string ="",
+        public contrasenia:string = ""
+    ){}
+}
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InicioSesionDTO } from '../../DTO/inicio-sesion-dto';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  InicioSesionDTO!: InicioSesionDTO;
+
+  username: string = '';
+  password: string = '';
+
+  constructor(){
+    this.InicioSesionDTO = new InicioSesionDTO();
+  }
+
+  onSubmit() {
+    
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 }
